b43a8bb2c718d902fc5bfeb52ba5104ad7bca8ef
varunkanneganti125 <varun.kanneganti@signitives.net>
2025-04-30 17:43:58 +0530

    changes to code

--- src/main/java/Demo/AWS/Controller/MyController.java
+++ src/main/java/Demo/AWS/Controller/MyController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
     @GetMapping("/")
     public String hello() {
-        return "Hello Varun";
+        return "Hello Ram!!!!";
     }
 }
