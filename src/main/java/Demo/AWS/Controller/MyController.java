5baf673a1a846bb58809a37e70c2bd788b05bc9a
varunkanneganti125 <varun.kanneganti@signitives.net>
2025-04-30 17:16:32 +0530

    changes to app controller file

--- src/main/java/Demo/AWS/Controller/MyController.java
+++ src/main/java/Demo/AWS/Controller/MyController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
     @GetMapping("/")
     public String hello() {
-        return "Deployment Completed";
+        return "Hello Varun";
     }
 }
