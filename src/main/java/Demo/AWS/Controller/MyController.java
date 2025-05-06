7e9a7c0dc9b2589062fb8298724600dcf06798fd
varunkanneganti125 <varun.kanneganti@signitives.net>
2025-05-06 10:18:05 +0530

    commit

--- src/main/java/Demo/AWS/Controller/MyController.java
+++ src/main/java/Demo/AWS/Controller/MyController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
     @GetMapping("/")
     public String hello() {
-        return "Hello varun, Jenkins Automation using webhook trigger is sucessfully running!!!!";
+        return "Hello varun, Jenkins Automation using webhook trigger is sucessfully running!!!";
     }
 }
